e73f890f91b6ba07cb775424f3267d268fd962f7
wilkemo <st143755@stud.uni-stuttgart.de>
2023-05-05 16:54:56 +0200

    black added

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'AtomCalc'
-copyright = '2023, Moritz Wilke'
-author = 'Moritz Wilke'
+project = "AtomCalc"
+copyright = "2023, Moritz Wilke"
+author = "Moritz Wilke"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ["sphinx_rtd_theme"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
