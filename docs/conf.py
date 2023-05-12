b593497fc9a7545778e6d96909b79c3bac9a756b
Moritz Wilke <st143755@stud.uni-stuttgart.de>
2023-05-12 13:16:45 +0200

    code added

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,13 @@ author = 'Moritz Wilke'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme"]
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
