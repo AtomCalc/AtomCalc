95c27f97626cf29c93655e9bf076b830dc706923
Moritz Wilke <st143755@stud.uni-stuttgart.de>
2023-06-21 22:35:48 +0200

    init

--- atomcalc/__init__.py
+++ atomcalc/__init__.py
@@ -1,5 +1,5 @@
 from atomcalc.Level import Level
-from atomcacl.Decay import Decay
+from atomcalc.Decay import Decay
 from atomcalc.System import System
 from atomcalc.plot_pulse import plot_pulse
 from atomcalc.Laser import Laser
